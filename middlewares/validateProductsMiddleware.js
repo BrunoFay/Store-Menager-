bc82cb11b3c68a3c1bc47a7df2cc81270e86e201
BRUNO <brunofay1@hotmail.com>
2022-03-31 03:01:42 -0300

    fix 3 lint e bug

--- middlewares/validateProductsMiddleware.js
+++ middlewares/validateProductsMiddleware.js
@@ -1,16 +1,21 @@
+const validateQuantity = (req, res, next) => {
+  const { quantity } = req.body;
+
+  if (!quantity && quantity !== 0) { 
+    return res.status(400).json({ message: '"quantity" is required' }); 
+}
+  if (quantity < 1) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+  next();
+};
 const validateProductMiddleware = (req, res, next) => {
-  const { name, quantity } = req.body;
-  console.log(req.body);
+  const { name } = req.body;
   if (!name) return res.status(400).json({ message: '"name" is required' });
-  if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
   if (name.length < 5) {
     return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
   }
-  if (quantity < 1) { 
-    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' }); 
-}
+    next();
+  };
 
-  next();
-};
-
-module.exports = validateProductMiddleware;
+  module.exports = [validateProductMiddleware, validateQuantity];
--- middlewares/validateSalesMiddleware.js
+++ middlewares/validateSalesMiddleware.js
@@ -1,13 +1,17 @@
 const validateSalesMiddleware = (req, res, next) => {
   const arrayProduct = req.body;
   arrayProduct.forEach((sale) => {
-    if (!sale.productId) return res.status(400).json({ message: '"productId" is required' });
-    if (!sale.quantity) return res.status(400).json({ message: '"quantity" is required' });
+    if (!sale.productId) {
+      return res.status(400).json({ message: '"productId" is required' });
+    }
+    if (!sale.quantity && sale.quantity !== 0) {
+      return res.status(400).json({ message: '"quantity" is required' }); 
+}
     if (sale.quantity < 1) {
       return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
     }
+    next();
   });
-  next();
 };
 
 module.exports = validateSalesMiddleware;
